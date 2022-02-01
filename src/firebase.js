5b45b38a247179db18dd053e6d7fb92bac613fdc
Aryan <aryanbiswari14@gmail.com>
2022-02-01 10:31:43 +0530

    make another redux application

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAmJc8mwPwjUeEOziQQ0o2Vo_RGY84FaNQ",
+  authDomain: "netflix-clone-49086.firebaseapp.com",
+  projectId: "netflix-clone-49086",
+  storageBucket: "netflix-clone-49086.appspot.com",
+  messagingSenderId: "963798106536",
+  appId: "1:963798106536:web:9bd45072ef7de140601d9d",
+  measurementId: "G-7T7RHFQP6C",
+};
