4083ad563ec0866e08dd9bb4890cef96cec7b6fa
Aryan <aryanbiswari14@gmail.com>
2022-02-19 19:07:40 +0530

    loginScreen completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React from 'react';
-// import logo from './logo.svg';
-// import { Counter } from './features/counter/Counter';
-import './App.css';
+import React from "react";
+import "./App.css";
 import HomeScreen from "./HomeScreen";
+import LoginScreen from "./LoginScreen";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const user = null;
   return (
     <div className="app">
-     <Router>
-        <Switch>
-          <Route path="/">
-            <HomeScreen />
-          </Route>
-        </Switch>
+      <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Switch>
+            <Route path="/">
+              <HomeScreen />
+            </Route>
+          </Switch>
+        )}
       </Router>
     </div>
   );
